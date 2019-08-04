1a6f4faabd69b883783a92445a70c8ce6001ab95
wilsenock220 <wilsenock220@gmail.com>
2019-08-04 10:49:33 +0300

    add register form

--- awards/urls.py
+++ awards/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url, include
+from django.contrib.auth import views as auth_views
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -6,8 +7,14 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^home/$', views.home, name='home'),
-
-
+    url(r'^upload/', views.upload_project, name='upload'),
+    url(r'^profile/(?P<username>\w+)', views.profile, name='profile'),
+    url(r'^accounts/update/', views.edit, name='update_profile'),
+    url(r'^search/', views.search_results, name='search_results'),
+    url(r'^project/(?P<project_id>\d+)', views.project, name='project'),
+    url(r'^rate/(\d+)', views.vote_project, name='rate'),
+    url(r'^api/profile/$', views.ProfileList.as_view()),
+    url(r'^api/project/$', views.ProjectList.as_view())
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
