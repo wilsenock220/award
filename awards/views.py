1c75da4abd70c100fd6a89bba02a05c57b1aa187
wilsenock220 <wilsenock220@gmail.com>
2019-08-04 10:56:41 +0300

    add vote view

--- awards/views.py
+++ awards/views.py
@@ -110,3 +110,35 @@ def project(request, project_id):
         'project': project,
         'rating': rating
     })
+
+
+def vote_project(request, project_id):
+    project = Project.objects.get(id=project_id)
+    rating = round(
+        ((project.design + project.usability + project.content) / 3), 2)
+    if request.method == 'POST':
+        form = VoteForm(request.POST)
+        if form.is_valid:
+            if project.design == 1:
+                project.design = int(request.POST['design'])
+            else:
+                project.design = (project.design +
+                                  int(request.POST['design'])) / 2
+            if project.usability == 1:
+                project.usability = int(request.POST['usability'])
+            else:
+                project.usability = (project.design +
+                                     int(request.POST['usability'])) / 2
+            if project.content == 1:
+                project.content = int(request.POST['content'])
+            else:
+                project.content = (project.design +
+                                   int(request.POST['content'])) / 2
+            project.save()
+    else:
+        form = VoteForm()
+    return render(request, 'vote.html', {
+        'form': form,
+        'project': project,
+        'rating': rating
+    })
