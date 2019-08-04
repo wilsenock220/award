0581a834ef2f124d0aca53eabb2483d99eba14e4
wilsenock220 <wilsenock220@gmail.com>
2019-08-04 10:54:04 +0300

    add profile view

--- awards/views.py
+++ awards/views.py
@@ -1,6 +1,13 @@
-from __future__ import unicode_literals
+from django.contrib.sites.shortcuts import get_current_site
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, Http404
-from django.shortcuts import render, get_object_or_404
+from .forms import *
+from .models import *
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 
 def index(request):
@@ -9,3 +16,34 @@ def index(request):
 
 def home(request):
     return render(request, 'home.html', locals())
+
+
+@login_required(login_url='/accounts/login/')
+def upload_project(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, request.FILES)
+        if form.is_valid():
+            add = form.save(commit=False)
+            add.save()
+            return redirect('home')
+    else:
+        form = ProjectForm()
+
+    return render(request, 'upload_project.html', locals())
+
+
+def profile(request, username):
+
+    profile = User.objects.get(username=username)
+    print(profile.id)
+    try:
+        profile_details = Profile.get_by_id(profile.id)
+    except:
+        profile_details = Profile.filter_by_id(profile.id)
+    user = request.user
+    profile = User.objects.get(username=username)
+    project = Project.objects.filter(owner=user)
+    title = f'@{profile.username} awwward projects and screenshots'
+
+    return render(request, 'profile.html', locals())
